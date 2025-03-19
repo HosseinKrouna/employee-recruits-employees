bfe59163211df3114a47de1db93c8de851f14831
Hossein.Krouna <hossein.krouna@gmx.net>
2025-03-19 12:49:21 +0100

    Initial commit – Setup: Spring Boot Project structur

---
+++ src/main/java/com/empfehlo/empfehlungsapp/EmpfehlungsappApplication.java
@@ -0,0 +1,13 @@
+package com.empfehlo.empfehlungsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpfehlungsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpfehlungsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empfehlo/empfehlungsapp/EmpfehlungsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empfehlo.empfehlungsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpfehlungsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
