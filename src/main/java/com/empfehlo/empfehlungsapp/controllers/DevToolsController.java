6886fc67c3ab2a70bdc924be3f5a8123374b9005
Hossein.Krouna <hossein.krouna@gmx.net>
2025-03-25 13:24:45 +0100

    modify: login auth

---
+++ src/main/java/com/empfehlo/empfehlungsapp/controllers/DevToolsController.java
@@ -0,0 +1,27 @@
+package com.empfehlo.empfehlungsapp.controllers;
+
+import com.empfehlo.empfehlungsapp.repositories.RecommendationRepository;
+import com.empfehlo.empfehlungsapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dev")
+public class DevToolsController {
+
+    @Autowired
+    private RecommendationRepository recommendationRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @DeleteMapping("/reset")
+    public ResponseEntity<String> resetAll() {
+        recommendationRepository.deleteAll();
+        userRepository.deleteAll();
+        return ResponseEntity.ok("✅ Alle Testdaten gelöscht.");
+    }
+}
+
