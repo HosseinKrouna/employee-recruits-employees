142756a3ab6403ae988e5f8cc32c10e5ef734998
Hossein.Krouna <hossein.krouna@gmx.net>
2025-03-26 09:21:56 +0100

    modify: upload initializer and uploadfile controller

--- src/main/java/com/empfehlo/empfehlungsapp/EmpfehlungsappApplication.java
+++ src/main/java/com/empfehlo/empfehlungsapp/EmpfehlungsappApplication.java
@@ -3,11 +3,14 @@ package com.empfehlo.empfehlungsapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class EmpfehlungsappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpfehlungsappApplication.class, args);
+
 	}
 
 }
